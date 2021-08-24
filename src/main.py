30f071c1f63b3f75839d7dd50b3ee5d6928a38c7
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-08-24 05:50:40 +0000

    Put endpoints in class

--- src/main.py
+++ src/main.py
@@ -8,23 +8,24 @@ from employee import Employee
 app = FastAPI()
 controller = Controller(ReplitDbEmployeeRepository())
 
-@app.get("/")
-async def read_root():
-    return {"Status": "Up"}
+class EmployeeEndpoint():
+  @app.get("/")
+  async def read_root():
+      return {"Status": "Up"}
 
-@app.get("/employees")
-async def list_all_employees():
-  return controller.get_all_employees()
+  @app.get("/employees")
+  async def list_all_employees():
+    return controller.get_all_employees()
 
-@app.get("/employees/{employee_id}")
-async def get_employee_by_id(employee_id: str):
-  try:
-    return controller.get_employee_by_id(employee_id)
-  except KeyError:
-    raise HTTPException(status_code=404, detail="Item not found")
+  @app.get("/employees/{employee_id}")
+  async def get_employee_by_id(employee_id: str):
+    try:
+      return controller.get_employee_by_id(employee_id)
+    except KeyError:
+      raise HTTPException(status_code=404, detail="Item not found")
 
-@app.put("/employees", status_code=201)
-def add_new_employee(name: str, employee_id: str):
-  controller.add_employee(Employee(name), employee_id)
+  @app.put("/employees", status_code=201)
+  def add_new_employee(name: str, employee_id: str):
+    controller.add_employee(Employee(name), employee_id)
 
 uvicorn.run(app,host="0.0.0.0",port=8080)
