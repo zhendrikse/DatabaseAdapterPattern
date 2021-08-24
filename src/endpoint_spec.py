99e0b49e0986b869350a47137778b3171fe9beb2
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-08-24 09:09:58 +0000

    Testing endpoint directly

---
+++ src/endpoint_spec.py
@@ -0,0 +1,37 @@
+from mamba import description, it, context, before
+from expects import expect, equal
+
+from fastapi import HTTPException
+
+from endpoint import EmployeeEndpoint
+from repository import StubEmployeeRepository
+from employee import Employee
+from controller import Controller
+
+with description(EmployeeEndpoint) as self:
+  with context("Given an employee endpoint"):
+    with before.each:
+      self.endpoint = EmployeeEndpoint(Controller(StubEmployeeRepository()))
+
+    with context("When inovking GET on /employees"):
+      with it("should return three"):
+        expect(
+          len(self.endpoint.list_all_employees())).to(equal(3))
+    
+    with context("When invoking GET on /employees with ID"):
+      with it("should return the employee with given ID"):
+        expect(
+          self.endpoint.get_employee_by_id("001")).to(equal(Employee("Zeger")))
+
+      with it("should throw an exception when ID does not exist"):
+        try:
+          self.endpoint.get_employee_by_id('xxx')
+          raise("Non-existing employee should not be found")
+        except HTTPException:
+          pass
+
+    with context("When invoking PUT on /employee"):
+      with it("should have four entries"):
+        self.endpoint.add_new_employee("Jan", "004")
+        expect(
+          len(self.endpoint.list_all_employees())).to(equal(4))
--- src/main.py
+++ src/main.py
@@ -1,38 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from fastapi_utils.cbv import cbv
-from fastapi_utils.inferring_router import InferringRouter
+from fastapi import FastAPI
 import uvicorn
 
-from repository import ReplitDbEmployeeRepository
-from controller import Controller
-from employee import Employee
-
-app = FastAPI()
-router = InferringRouter()
-
-@cbv(router) 
-class EmployeeEndpoint:
-    def __init__(self, custom_controller = Controller(ReplitDbEmployeeRepository())):
-      self.controller = custom_controller
-
-    @router.get("/")
-    def ping(self) -> str:
-       return 'Pong'
-
-    @router.get("/employees")
-    async def list_all_employees(self):
-      return self.controller.get_all_employees()
-
-    @router.get("/employees/{employee_id}")
-    async def get_employee_by_id(self, employee_id: str):
-      try:
-        return self.controller.get_employee_by_id(employee_id)
-      except KeyError:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-    @router.put("/employees", status_code=201)
-    def add_new_employee(self, name: str, employee_id: str):
-      self.controller.add_employee(Employee(name), employee_id)
+from endpoint import app, router
 
 app.include_router(router)
 uvicorn.run(app,host="0.0.0.0",port=8080)
